668447939275cca4f00735182839895e6e02b70b
Pan C.A <2358797100@qq.com>
2023-05-19 00:26:48 +0800

    update

--- Tool/nameEditGUI.tcl
+++ Tool/nameEditGUI.tcl
@@ -12,7 +12,7 @@ namespace eval ::nameGUI {
 }
 
 if {[info exists ::nameGUI::sheet]==0}		{set ::nameGUI::sheet 0}
-if {[info exists ::nameGUI::currentModule]==0}		{set ::nameGUI::currentModule 添加前缀}
+if {[info exists ::nameGUI::currentModule]==0}		{set ::nameGUI::currentModule 添加文本}
 
 set width [expr {$panelWidth / $::nameGUI::button_cloumn / 10 + 1}]
 set ::nameGUI::button_width $width
@@ -20,12 +20,43 @@ set ::nameGUI::filepath [file dirname [info script]]
 set ::nameGUI::label_width $::nameGUI::button_width
 set ::nameGUI::name "名称编辑"
 
+# 创建按键阵列：函数{位置，列表}
+proc create_label_button {loc line} {
+
+	set line_title [lindex $line 0]
+	set line_button [lrange $line 1 end]
+
+	label .f.top.$loc.0  -text "$line_title" -width $::nameGUI::label_width -font {MS 10}  -compound center -height 1 -fg #A00000
+	set num [llength $line_button]
+	set n_cur 1
+	foreach button_data $line_button {
+		set name [lindex $button_data 0]
+		set prefix [string index $name 0]
+		if {$prefix == "_"} {
+			set BTstate disabled
+		} else {
+			set BTstate normal
+		}
+		set file_command [lindex $button_data 1]
+		button .f.top.$loc.$n_cur \
+		-text "$name" \
+		-command [format "source %s/%s" $::nameGUI::filepath $file_command]\
+		-state $BTstate\
+		-background #F0F0F0 \
+		-fg #000000 \
+		-height 1\
+		-width $::nameGUI::button_width\
+		-font {MS 10}\
+		-relief groove
+		if {$n_cur==$num} { break }
+		set n_cur [expr $n_cur+1]
+	}
+}
+
 proc pushPanel {module} {
-	if {$module=="添加前缀"} { set ::nameGUI::sheet 0}
-	if {$module=="添加后缀"} { set ::nameGUI::sheet 1}
-	if {$module=="删除前缀"} { set ::nameGUI::sheet 2}
-	if {$module=="删除后缀"} { set ::nameGUI::sheet 3}
-	if {$module=="替换文本"} { set ::nameGUI::sheet 3}
+	if {$module=="添加文本"} { set ::nameGUI::sheet 0}
+	if {$module=="删除文本"} { set ::nameGUI::sheet 1}
+	if {$module=="替换文本"} { set ::nameGUI::sheet 2}
 	creatNamePanel $::nameGUI::sheet
 }
 
@@ -39,7 +70,79 @@ proc creatNamePanel {sheet} {
 		frame .f.top.$j
 		pack .f.top.$j -side left -fill x -anchor nw -padx 0 -pady 0
 	}
-
+	
+	# 第一页：添加文本
+	if {$sheet==0} {
+		set 	line "前缀"
+		lappend line "{Comps} {hmCompNameEdit.tcl;nameEdit comps addFront}"
+		lappend line "{Materials} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Props} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Groups} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Loads} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Sets} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		create_label_button 1 $line
+		
+		set 	line "后缀"
+		lappend line "{Comps} {hmCompNameEdit.tcl;nameEdit comps addRear}"
+		lappend line "{Materials} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Props} {hmCompNameEdit.tcl;nameEdit props addRear}"
+		lappend line "{Groups} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Loads} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Sets} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		create_label_button 2 $line
+		}
+		
+	# 第二页：删除文本
+	if {$sheet==1} {
+		set 	line "前缀"
+		lappend line "{Comps} {hmCompNameEdit.tcl;nameEdit comps addFront}"
+		lappend line "{Materials} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Props} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Groups} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Loads} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Sets} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		create_label_button 1 $line
+		
+		set 	line "后缀"
+		lappend line "{Comps} {hmCompNameEdit.tcl;nameEdit comps addFront}"
+		lappend line "{Materials} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Props} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Groups} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Loads} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Sets} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		create_label_button 2 $line
+		
+		set 	line "任意位置"
+		lappend line "{Comps} {hmCompNameEdit.tcl;nameEdit comps addFront}"
+		lappend line "{Materials} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Props} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Groups} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Loads} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Sets} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		create_label_button 3 $line
+		}
+		
+	# 第三页：替换文本
+	if {$sheet==2} {
+		set 	line "任意位置"
+		lappend line "{Comps} {hmCompNameEdit.tcl;nameEdit comps addFront}"
+		lappend line "{Materials} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Props} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Groups} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Loads} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		lappend line "{Sets} {hmCompNameEdit.tcl;nameEdit props addFront}"
+		create_label_button 1 $line
+		}	
+	
+		
+	# pack小部件设置
+	for { set hloc 0 } { $hloc < 8 } { incr hloc 1 } {
+		for { set vloc 1 } { $vloc < 15 } { incr vloc 1 } {
+			catch {
+				pack .f.top.$vloc.$hloc -side top -anchor nw -padx 0 -pady 0
+			}
+		}
+	}
 }
 
 # 初始设置
@@ -49,7 +152,7 @@ frame .f.side
 pack .f.side -side left -fill both
 
 set i 0
-foreach module {添加前缀 添加后缀 删除前缀 删除后缀 替换文本} {
+foreach module {添加文本 删除文本 替换文本} {
     pack [radiobutton .f.side.$i -text $module -variable ::nameGUI::currentModule \
         -value $module \
 		-height 1\
